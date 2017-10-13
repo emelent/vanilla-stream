23ee737e112164b028821504be1c07e68216e322
Roy the Mortal <technicmk@gmail.com>
2017-10-13 16:15:12 +0200

    Implement movies app in baconjs

--- movies-bacon.js
+++ movies-bacon.js
@@ -2,30 +2,41 @@
 // STREAMS
 //==================
 // on keyup event stream
-const onKeyUp = Bacon.fromEvent($('#item-text'), 'keyup')
+const onKeyUp = Bacon.fromEvent($('#search-text'), 'keyup')
 
-// on list item click(remove item)
-const onListMovieClick = Bacon.fromEvent($('#movie-list'), 'click')
+// on movie list click stream
+const onListClick = Bacon.fromEvent($('#movie-list'), 'click')
 
-//text value stream
-const textValue = onKeyUp.map(e => e.target.value).debounce(800)
+// on back button click stream
+const onBackClick = Bacon.fromEvent($('#back-button'), 'click')
 
-const loadMovies = Bacon.fromPromise(fetch('./movies.json'))
+// text value stream
+const onTextValue = onKeyUp.map(e => e.target.value).debounce(800)
+
+// load movies stream
+const onLoadMovies = Bacon.fromPromise(fetch('./movies.json'))
 	.flatMap(result => Bacon.fromPromise(result.json()))
 
+// on movie select stream
+const onSelectedMovie  = onListClick.map(e => e.target.dataset.title)
 
 //==================
 // REDUCERS
 //==================
 
-//search movies reducer
+// search movies reducer
 const searchMoviesReducer = (state, text) => Object.assign({}, state, {
-		results: searchMovies(state.movies, text)
+		results: searchMovies(state.movies, text),
+		currentMovie: null //set current movie to null every time search is performed
 	})
 
-//load movies reducer
-const loadMoviesReducer = (state, movies) => Object.assign({}, state, {movies})
+// load movies reducer
+const onLoadMoviesReducer = (state, movies) => Object.assign({}, state, {movies})
 
+// select movie reducer
+const selectMovieReducer = (state, title) => Object.assign({}, state, {
+	currentMovie: getMovie(state.movies, title)
+})
 
 //==================
 // STORE
@@ -33,16 +44,38 @@ const loadMoviesReducer = (state, movies) => Object.assign({}, state, {movies})
 
 const store = Bacon.update({
 		movies: [],
-		results: []
+		results: [],
+		currentMovie: null
 	},
-	textValue, searchMoviesReducer,
-	loadMovies, loadMoviesReducer
+	onTextValue, searchMoviesReducer,
+	onLoadMovies, onLoadMoviesReducer,
+	onSelectedMovie,  selectMovieReducer
 )
 
 //==================
 // SIDE EFFECTS
 //==================
 
+// stream for when current movie is updated to non-null
+const onSetCurrentMovie = store.map(state => state.currentMovie).filter(x => x)
+
 // render list on update
 store.onValue(({results}) => hideSpinner() || renderList(results))
+
+// show movie
+onSetCurrentMovie.onValue(showMovie)
+
+// show poster loader
+onSetCurrentMovie
+	.onValue(showImageLoader)
+
+// update image src url
+onSetCurrentMovie
+	.flatMap(movie => Bacon.fromPromise(fetch(movie.posterurl)))
+	.onValue(response => updatePosterUrl(response.url))
+
+// show spinner on key up(pretending to search)
 onKeyUp.onValue(showSpinner)
+
+// go back to movie list when back button clicked
+onBackClick.onValue(hideMovie)
--- movies-standard.js
+++ movies-standard.js
@@ -1,76 +1,17 @@
-//=========================
-// GLOBALS
-//=========================
+let blockSearch = false
+let movies = []
+let results = []
 
-//item list
-let items = []
-
-//=========================
-// EVENT HANDLERS
-//=========================
-
-// on button click event handler
-$('#add-button').onclick =  () => {
-	// add item and re-render list
-	addAndUpdate(getText())
+$('#search-text').onkeyup = e => {
+	if(blockSearch)	return
+	showSpinner()
+	const text = e.target.value
+	blockSearch = true
+	setTimeout(() => {
+		blockSearch = false
+		results = searchMovies(movies, text)
+		renderList(results)
+		hideSpinner()
+	}, 800)
 }
-
-//on key up event handler
-$('#item-text').onkeyup = e => {
-	// input box text
-	const text = getText()
-	
-	if(nonEmpty(text)){
-		// update btn enabled
-		setDisabled($('#add-button'), false)
-
-		// handle on enter key press
-		if(e.keyCode === 13){
-			// add item and re-render list
-			addAndUpdate(text)
-		}
-	}else{
-		// update btn disabled
-		setDisabled($('#add-button'), true)
-	}
-}
-
-// on list item click
-$('#items-list').onclick = e =>  {
-	// remove item from list
-	removeItem(e.target.dataset.id)
-
-	// re-render list
-	renderList(items)
-}
-
-
-//=========================
-// FUNCS
-//=========================
-
-// adds new item && updates list 
-const addAndUpdate = text => {
-	// add item to list
-	addItem(text)
-
-	// clear text input
-	clearTextInput()
-
-	// re-render list
-	renderList(items)
-} 
-
-// add new item to list
-const addItem = text => items.push(createItem(text))
-
-// remove item from list
-const removeItem = id => items = items.filter(x => x.id != id)
-
-
-//=========================
-// SET UP
-//=========================
-
-// enable/disable button on start up
-$('#add-button').disabled = !nonEmpty(getText())
+fetch('./movies.json').then(result => result.json()).then(data => movies = data)
